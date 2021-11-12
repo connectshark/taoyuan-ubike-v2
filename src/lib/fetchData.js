ba28a6829f42f0da7e6eaa58bb67813e28e25340
Chambers <46649021+connectshark@users.noreply.github.com>
2021-11-12 23:14:25 +0800

    使用正式資料

--- src/lib/fetchData.js
+++ src/lib/fetchData.js
@@ -1,18 +1,18 @@
 const fetchData = {
-  getStation () {
-    return new Promise(resolve => {
-      fetch('bike.json').then(r => r.json()).then(res => {
-        resolve(res)
-      })
-    })
-  },
-  getBikeStatus () {
-    return new Promise(resolve => {
-      fetch('bikeState.json').then(r => r.json()).then(res => {
-        resolve(res)
-      })
-    })
-  },
+  // getStation () {
+  //   return new Promise(resolve => {
+  //     fetch('bike.json').then(r => r.json()).then(res => {
+  //       resolve(res)
+  //     })
+  //   })
+  // },
+  // getBikeStatus () {
+  //   return new Promise(resolve => {
+  //     fetch('bikeState.json').then(r => r.json()).then(res => {
+  //       resolve(res)
+  //     })
+  //   })
+  // },
   getRoute () {
     return new Promise(resolve => {
       fetch('route.json').then(r => r.json()).then(res => {
@@ -20,20 +20,20 @@ const fetchData = {
       })
     })
   },
-  // getStation () {
-  //   return new Promise(resolve => {
-  //     fetch(process.env.VUE_APP_API_URL + `Station/Taoyuan` + `?$format=JSON`).then(r => r.json()).then(res => {
-  //       resolve(res)
-  //     })
-  //   })
-  // },
-  // getBikeStatus () {
-  //   return new Promise(resolve => {
-  //     fetch(process.env.VUE_APP_API_URL + `Availability/Taoyuan` + `?$format=JSON`).then(r => r.json()).then(res => {
-  //       resolve(res)
-  //     })
-  //   })
-  // }
+  getStation () {
+    return new Promise(resolve => {
+      fetch(process.env.VUE_APP_API_URL + `Station/Taoyuan` + `?$format=JSON`).then(r => r.json()).then(res => {
+        resolve(res)
+      })
+    })
+  },
+  getBikeStatus () {
+    return new Promise(resolve => {
+      fetch(process.env.VUE_APP_API_URL + `Availability/Taoyuan` + `?$format=JSON`).then(r => r.json()).then(res => {
+        resolve(res)
+      })
+    })
+  }
 }
 
 export default fetchData
